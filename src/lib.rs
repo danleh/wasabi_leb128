64fa11fec7f366eb23e9331aba20384606992f9e
Daniel Lehmann <mail@dlehmann.eu>
2019-09-26 14:30:22 +0200

    implement WriteLeb128 trait based on num_traits, not macro, minor clarifications, WIP: ReadLeb128

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
+use std::error;
+use std::fmt;
 use std::io;
 
-use byteorder::{ReadBytesExt, WriteBytesExt};
+use byteorder::ReadBytesExt;
+use num_traits::*;
 
 #[cfg(test)]
 mod tests;
@@ -13,7 +16,32 @@ pub trait ReadLeb128<T>: io::Read {
 
 pub trait WriteLeb128<T>: io::Write {
     /// Returns the actual written byte count.
-    fn write_leb128(&mut self, value: T) -> io::Result<usize>;
+    fn write_leb128(&mut self, value: T) -> Result<usize, Leb128Error>;
+}
+
+#[derive(Debug)]
+pub enum Leb128Error {
+    Overflow,
+    UnexpectedEof(io::Error),
+    Other(io::Error),
+}
+
+impl fmt::Display for Leb128Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
+        unimplemented!() // FIXME
+    }
+}
+
+impl error::Error for Leb128Error {}
+
+impl From<io::Error> for Leb128Error {
+    fn from(e: io::Error) -> Self {
+        match e.kind() {
+            // TODO better error message, more data?
+            io::ErrorKind::UnexpectedEof => Leb128Error::UnexpectedEof(e),
+            _ => Leb128Error::Other(e),
+        }
+    }
 }
 
 /* Trait implementations */
@@ -21,20 +49,36 @@ pub trait WriteLeb128<T>: io::Write {
 const CONTINUATION_BIT: u8 = 0x80;
 
 #[inline]
-fn is_set_continuation_bit(byte: u8) -> bool {
+fn continuation_bit(byte: u8) -> bool {
     byte & CONTINUATION_BIT == CONTINUATION_BIT
 }
 
 const SIGN_BIT: u8 = 0x40;
 
 #[inline]
-fn is_set_sign_bit(byte: u8) -> bool {
+fn sign_bit(byte: u8) -> bool {
     byte & SIGN_BIT == SIGN_BIT
 }
 
 macro_rules! signed {
-    ($T: ident) => ($T::min_value() != 0)
+    ($T: ident) => {
+        $T::min_value() != 0
+    };
 }
+
+// For inspiration, see
+// Wiki: https://en.wikipedia.org/wiki/LEB128
+// Official DWARF spec, annex C: http://dwarfstd.org/doc/dwarf-2.0.0.pdf
+// LLVM implementation: http://llvm.org/doxygen/LEB128_8h_source.html
+//      NOTE decodesSLEB128 seems to have no overflow checking!?
+//      Only 128bits?
+// V8 implementation: https://github.com/v8/v8/blob/4b9b23521e6fd42373ebbcb20ebe03bf445494f9/src/wasm/decoder.h#L329
+//      NOTE the template-based unrolling!
+//      Signed and unsigned, and different sizes in one function template
+// parity-wasm implementation: https://github.com/paritytech/parity-wasm/blob/master/src/elements/primitives.rs#L35
+// leb128 crate: https://github.com/gimli-rs/leb128
+// WebAssembly spec (for signed vs. unsigned vs. "uninterpreted"): https://webassembly.github.io/spec/core/binary/values.html
+// About shift overflow detection: https://blogs.msdn.microsoft.com/xiangfan/2009/06/13/detect-shift-overflow/
 
 // Need to write this as a macro, not a generic impl because num_traits are quite lacking, e.g.,
 // there is no "U as T" for primitive integers.
@@ -49,15 +93,43 @@ macro_rules! impl_leb128_integer {
                 let mut shift = 0;
                 let mut byte = CONTINUATION_BIT;
 
-                while is_set_continuation_bit(byte) {
+
+                const T_size_bits: usize = std::mem::size_of::<$T>() * 8;
+                // see https://stackoverflow.com/questions/2745074/fast-ceiling-of-an-integer-division-in-c-c
+                const fn int_div_ceil(x: usize, y: usize) -> usize {
+                    x/y + (x % y != 0) as usize
+                }
+                const T_max_leb128_bytes: usize = int_div_ceil(T_size_bits, 7);
+//                println!("\nT: {}\nT_size_bits: {}\nT_max_leb128_bytes: {}\n", stringify!($T), T_size_bits, T_max_leb128_bytes);
+
+
+                while continuation_bit(byte) {
                     // TODO better error message if ErrorKind::UnexpectedEof -> LEB128 unexpected end
                     byte = self.read_u8().map_err(|e| io::Error::new(
                                 io::ErrorKind::InvalidData,
                                 format!("unexpected end of LEB128 data after {} bytes read, current value: {}\ncaused by:{}", _bytes_read, value, e)))?;
 
                     // mask off continuation bit from byte and prepend lower 7 bits to value
-                    if let Some(high_bits) = ((byte & !CONTINUATION_BIT) as $T).checked_shl(shift) {
-                        value |= high_bits;
+                    // FIXME checked_shl does actually NOT check for overflow of the shifted value,
+                    // only that the shift-number is smaller than the number of bits of the target type
+                    // NOTE x86 masks off shift > N bits!
+                    let low_bits = (byte & !CONTINUATION_BIT) as $T;
+
+                    // TODO add warning for overflow of shift bits
+
+
+                    let current_byte = shift as usize / 7;
+                    let is_last_byte = current_byte == T_max_leb128_bytes - 1;
+
+//                    println!("byte {:x}\ncurrent_byte {}\nis_last_byte {}", byte, current_byte, is_last_byte);
+
+//                    use num_traits::PrimInt;
+//                    let overflow = is_last_byte && ((low_bits.wrapping_shl(shift)).wrapping_shr(shift)) != low_bits;
+                    let overflow = current_byte > T_max_leb128_bytes;
+
+                    if !overflow {
+                        value |= low_bits.wrapping_shl(shift);
+//                        println!("shift {}\nlow_bits {:x}\n<<    {:x}\n<< >> {:x}\nvalue {:x}\n", shift, low_bits, low_bits.wrapping_shl(shift), low_bits.wrapping_shl(shift).wrapping_shr(shift), value);
                     } else {
                         return Err(io::Error::new(io::ErrorKind::InvalidData, format!("LEB128 to {} overflow", stringify!($T))));
                     }
@@ -67,37 +139,57 @@ macro_rules! impl_leb128_integer {
 
                 if signed!($T) {
                     let (sign_extend, did_overflow) = (!0 as $T).overflowing_shl(shift);
-                    if is_set_sign_bit(byte) && !did_overflow {
+                    if sign_bit(byte) && !did_overflow {
+//                    let sign_extend = (!0 as $T).wrapping_shl(shift-7+1);
+//                    if is_set_sign_bit(byte) {
                         value |= sign_extend;
                     }
+
+//                    println!("extension bits {:x}, shift {}\nsign extended {:x}", sign_extend, shift, value);
                 }
+
 
                 Ok(value)
             }
         }
 
-        impl<W: io::Write> WriteLeb128<$T> for W {
-            fn write_leb128(&mut self, mut value: $T) -> io::Result<usize> {
-                let mut bytes_written = 0;
-                let mut more_bytes = true;
+    }
+}
 
-                while more_bytes {
-                    // select low 7 bits of value
-                    let mut byte_to_write = value as u8 & !CONTINUATION_BIT;
-                    // sign extends, important for signed integers!
-                    value >>= 7;
-                    bytes_written += 1;
+/// Mask off the continuation bit from the byte (= extract only the last 7, meaningful LEB128 bits).
+#[inline]
+fn non_continuation_bits(byte: u8) -> u8 {
+    byte & !CONTINUATION_BIT
+}
 
-                    more_bytes = value != if signed!($T) && is_set_sign_bit(byte_to_write) { !0 } else { 0 };
-                    if more_bytes {
-                        byte_to_write |= CONTINUATION_BIT;
-                    }
-                    self.write_u8(byte_to_write)?;
-                }
+// Combined implementation for signed and unsigned primitive integers.
+impl<W, T> WriteLeb128<T> for W
+where
+    W: io::Write,
+    T: PrimInt + AsPrimitive<u8>,
+{
+    fn write_leb128(&mut self, mut value: T) -> Result<usize, Leb128Error> {
+        let is_negative = value < T::zero();
+        let empty_value = if is_negative { !T::zero() } else { T::zero() };
 
-                Ok(bytes_written as usize)
+        let mut bytes_written = 0;
+        let mut more_bytes = true;
+
+        while more_bytes {
+            let mut byte_to_write = non_continuation_bits(value.as_());
+            // Shr sign-extends for signed types and is logical shift for unsigned types.
+            value = value.shr(7);
+
+            more_bytes = value != empty_value;
+            if more_bytes {
+                byte_to_write |= CONTINUATION_BIT;
             }
+
+            self.write_all(&[byte_to_write])?;
+            bytes_written += 1;
         }
+
+        Ok(bytes_written)
     }
 }
 
