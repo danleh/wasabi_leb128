4e7f853da5abeada10e60363e7f1657736c069f4
Daniel Lehmann <mail@dlehmann.eu>
2019-09-27 16:07:43 +0200

    implement more overflow checking tests, cleanup

--- src/tests.rs
+++ src/tests.rs
@@ -1,29 +1,30 @@
 use crate::{ReadLeb128, WriteLeb128};
 
-use std::io;
-use std::{i16, i32, u16};
-use std::convert::TryFrom;
+use std::{i16, i32, u16, u32};
 
-// Utility function that gives back a vector of bytes with the LEB128 representation of x.
+/// Utility function that gives back a vector of bytes with the LEB128 representation of x.
 fn write_leb128<T>(x: T) -> Vec<u8>
 where
     Vec<u8>: WriteLeb128<T>,
 {
     let mut buf = Vec::new();
-    buf.write_leb128(x).expect("writing to a vector should never fail, since all input values can be represented in LEB128");
+    buf.write_leb128(x).expect("writing to a vector should never fail, since all input values of an integer type T can be represented in LEB128");
     buf
 }
 
+// Test some manually selected values encode to the right LEB128 bytes.
+
 #[test]
-fn known_values() {
-    // Unsigned numbers:
+fn known_encodings_unsigned() {
     assert_eq!(write_leb128(0_u32), [0x00]);
     assert_eq!(write_leb128(1_u32), [0x01]);
     assert_eq!(write_leb128(127_u32), [0x7f]);
     assert_eq!(write_leb128(128_u32), [0x80, 0x01]);
     assert_eq!(write_leb128(16256_u32), [0x80, 0x7f]);
+}
 
-    // Signed numbers:
+#[test]
+fn known_encoding_signed() {
     assert_eq!(write_leb128(0_i32), [0x00]);
     assert_eq!(write_leb128(1_i32), [0x01]);
     assert_eq!(write_leb128(-1_i32), [0x7f]);
@@ -33,28 +34,67 @@ fn known_values() {
     assert_eq!(write_leb128(-128_i32), [0x80, 0x7f]);
 }
 
+// Exhaustively test that decode(encode(value)) == value for u16 and i16.
+
 #[test]
-/// Exhaustively test that decode(encode(value)) == value for u16 and i16.
-fn roundtrips() -> Result<(), io::Error> {
+fn roundtrips_u16() {
     for u in u16::MIN..=u16::MAX {
         let buf = write_leb128(u);
-        let u_decode: u16 = buf.as_slice().read_leb128()?;
-
+        let u_decode: u16 = buf
+            .as_slice()
+            .read_leb128()
+            .expect("valid u16 should be possible to decode");
         assert_eq!(u, u_decode, "\nLEB128 bytes: {:?}", buf);
     }
-
-    for i in i16::MIN..=i16::MAX {
-        let buf = write_leb128(i);
-        let i_decode: i16 = buf.as_slice().read_leb128()?;
-
-        assert_eq!(i, i_decode, "\nLEB128 bytes: {:?}", buf);
-    }
-
-    Ok(())
 }
 
 #[test]
-fn overflow_is_detected() -> Result<(), std::io::Error> {
+fn roundtrips_i16() {
+    for i in i16::MIN..=i16::MAX {
+        let buf = write_leb128(i);
+        let i_decode: i16 = buf
+            .as_slice()
+            .read_leb128()
+            .expect("valid i16 should be possible to decode");
+        assert_eq!(i, i_decode, "\nLEB128 bytes: {:?}", buf);
+    }
+}
+
+// Check for the existence of errors when parsing a LEB128 value that doesn't fit in the target type.
+
+/// Utility macro to check for overflow errors when converting from a wider to a narrow type.
+macro_rules! detect_overflow {
+    ($original_value: expr, $T: ty, $U: ty) => ({
+        // Encode as the wider type.
+        let original_value: $T = $original_value;
+        let buf = write_leb128(original_value);
+
+        // Decode as the narrower type.
+        let decoded_value: Result<$U, _> = buf.as_slice().read_leb128();
+
+        // If the input value cannot be represented without loss in the target type,
+        // read_leb128() should have Err()'d.
+        use std::convert::TryFrom;
+        let expect_overflow = <$U>::try_from(original_value).is_err();
+        if expect_overflow {
+            assert!(
+                decoded_value.is_err(),
+                "expected overflow error when decoding {} {:?} ({:?} in LEB128) as {}, but got {:?}",
+                stringify!($T),
+                original_value,
+                buf,
+                stringify!($U),
+                decoded_value
+            );
+        } else {
+            assert_eq!(original_value, decoded_value.unwrap() as $T);
+        }
+    })
+}
+
+#[test]
+fn overflow_is_detected_i32_to_i16() {
+    // We cannot exhaustively test all 32bit numbers, so let's pick some special values:
     let values = [
         // Those two overflow because they use more bytes than possible for an i16 (5 instead of max 3).
         i32::MIN,
@@ -77,28 +117,41 @@ fn overflow_is_detected() -> Result<(), std::io::Error> {
         i32::MAX,
     ];
 
-    for &i_original in &values {
-        // Encode as 32 bit.
-        let buf = write_leb128(i_original);
-
-        // Decode as 16 bit.
-        let i_decode: Result<i16, _> = buf.as_slice().read_leb128();
-
-        // If the input value cannot be represented without loss in the target type,
-        // read_leb128() should have Err()'d.
-        let overflow = i16::try_from(i_original).is_err();
-        if overflow {
-            assert!(
-                i_decode.is_err(),
-                "expected overflow when decoding i32 {} ({:?} in LEB128) as i16, but got {:?}",
-                i_original,
-                buf,
-                i_decode
-            );
-        } else {
-            assert_eq!(i_original, i_decode? as i32);
-        }
+    for &i in &values {
+        detect_overflow!(i, i32, i16)
     }
+}
 
-    Ok(())
+#[test]
+fn overflow_is_detected_u32_to_u16() {
+    // Similar to the test case above, just without the negative numbers.
+    let values = [
+        0,
+        1,
+        u16::MAX as u32 - 1,
+        u16::MAX as u32,
+        u16::MAX as u32 + 1,
+        u32::MAX - 1,
+        u32::MAX,
+    ];
+
+    for &u in &values {
+        detect_overflow!(u, u32, u16)
+    }
+}
+
+#[test]
+fn overflow_is_detected_i16_to_i8() {
+    // For 16-bit values, we can test overflow detection exhaustively.
+    for i in i16::MIN..=i16::MAX {
+        detect_overflow!(i, i16, i8)
+    }
+}
+
+#[test]
+fn overflow_is_detected_u16_to_u8() {
+    // Same as above, just for all unsigned 16-bit integers.
+    for u in u16::MIN..=u16::MAX {
+        detect_overflow!(u, u16, u8)
+    }
 }
